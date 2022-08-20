c155524881463934634a0f3640b91244a168c92e
PawelDat <pawel.datko2000@gmail.com>
2022-08-20 15:56:02 +0200

    Completed Structure

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {MenuIcon,XIcon} from '@heroicons/react/outline'
+const NavBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/Priciing.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Priciing = () => {
+  return (
+    <div>Priciing</div>
+  )
+}
