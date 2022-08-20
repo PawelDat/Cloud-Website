1f7d2ef001d1bbd4df8cd2540bc1473cf65928f4
PawelDat <pawel.datko2000@gmail.com>
2022-08-20 16:53:17 +0200

    Completed Section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Hero from './components/Hero'
 import NavBar from './components/NavBar'
 import AboutUs from './components/AboutUs'
+import Support from './components/Support'
 const App = () => {
   return (
     <div className="app">
       <NavBar />
       <Hero/>
       <AboutUs/>
-    
+      <Support/>
     </div>
   )
 }
--- src/components/Support.js
+++ src/components/Support.js
@@ -1,7 +1,81 @@
-import React from 'react'
+import React from "react";
 
-export const Support = () => {
+import { PhoneIcon, ArrowSmRightIcon } from "@heroicons/react/outline";
+import { CheckIcon, SupportIcon } from "@heroicons/react/solid";
+
+import supportImg from "../assets/support.jpg";
+
+const Support = () => {
   return (
-    <div>Support</div>
-  )
-}
+    <div name="support" className="w-full mt-24">
+      <div className="w-full h-[700px] bg-gray-900/90 absolute">
+        <img
+          className="w-full h-full object-cover mix-blend-overlay"
+          src={supportImg}
+          alt="/"
+        />
+      </div>
+
+      <div className="max-w-[1240px] mx-auto text-white relative">
+        <div className="px-4 py-12">
+          <h2 className="text-3xl pt-8 text-slate-300 uppercase text-center">
+            Support
+          </h2>
+          <h3 className="text-5xl font-bold py-6 text-center">
+            Finding the right team
+          </h3>
+          <p className="py-4 text-3xl text-slate-300">
+          we will provide you with the best technical support through our specialized employees
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black">
+          <div className="bg-white rounded-xl shadow-2xl">
+            <div className="p-8">
+              <PhoneIcon className="w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]" />
+              <h3 className="font-bold text-2xl my-6">Sales</h3>
+              <p className="text-gray-600 text-xl">
+              Our sales support are always free to provide you the best Cloud options for you
+              </p>
+            </div>
+            <div className="bg-slate-100 pl-8 py-4">
+              <p className="flex items-center text-indigo-600">
+                Contact Us <ArrowSmRightIcon className="w-5 ml-2" />
+              </p>
+            </div>
+          </div>
+          <div className="bg-white rounded-xl shadow-2xl">
+            <div className="p-8">
+              <SupportIcon className="w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]" />
+              <h3 className="font-bold text-2xl my-6">Technical Support</h3>
+              <p className="text-gray-600 text-xl">
+              By purchasing our product we provide you with 24/7 technical support
+              </p>
+            </div>
+            <div className="bg-slate-100 pl-8 py-4">
+              <p className="flex items-center text-indigo-600">
+                Contact Us <ArrowSmRightIcon className="w-5 ml-2" />
+              </p>
+            </div>
+          </div>
+          <div className="bg-white rounded-xl shadow-2xl">
+            <div className="p-8">
+              <CheckIcon className="w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]" />
+              <h3 className="font-bold text-2xl my-6">Free Trial</h3>
+              <p className="text-gray-600 text-xl">
+              Feel Free to checkout our 14 days free trial to consider wich options suits you the best.
+              </p>
+            </div>
+            <div className="bg-slate-100 pl-8 py-4">
+              <p className="flex items-center text-indigo-600">
+                Contact Us <ArrowSmRightIcon className="w-5 ml-2" />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
