3802eb03ac2d13bb1e877eb407691de98f22c0cd
PawelDat <pawel.datko2000@gmail.com>
2022-08-20 17:16:24 +0200

    Completed Section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import Hero from './components/Hero'
-import NavBar from './components/NavBar'
-import AboutUs from './components/AboutUs'
-import Support from './components/Support'
-import SupportDetails from './components/SupportDetails'
+import React from "react";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import AboutUs from "./components/AboutUs";
+import Support from "./components/Support";
+import SupportDetails from "./components/SupportDetails";
+import Pricing from "./components/Pricing";
+
 const App = () => {
   return (
     <div className="app">
       <NavBar />
-      <Hero/>
-      <AboutUs/>
-      <Support/>
-      <SupportDetails/>
+      <Hero />
+      <AboutUs />
+      <Support />
+      <SupportDetails />
+      <Pricing />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Priciing.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Priciing = () => {
-  return (
-    <div>Priciing</div>
-  )
-}
